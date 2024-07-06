38812ddbc9cf8c59abeb10f818c13bf3686c0a61
Umerwaseem4 <umerwas4@gmail.com>
2024-07-06 20:55:26 +0500

    added layout to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import { Layout } from "./components/layout/Layout";
 import { Button } from "./components/ui/button";
 
 function App() {
   return (
     <>
-      <h1>Hello World</h1>
-      <Button>Click me!</Button>
+      <Layout>
+        <h1>THis is layout of the app!</h1>
+        <Button>Click me</Button>
+      </Layout>
     </>
   );
 }
