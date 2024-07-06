23c4a067e58a3583926bddf35179497246076a5d
Umerwaseem4 <umerwas4@gmail.com>
2024-07-06 08:48:44 +0500

    project setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1>Hello World</h1>
+      <Button>Click me!</Button>
+    </>
+  );
+}
+
+export default App;
